bfdc6cea3da02cd8023f75120892c5fbaa3718b7
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-01-26 16:10:43 -0300

    Seccion 02: Clase 09 - Paths, servicios y guards

--- 01-clitest/src/app/pages/home/home.component.ts
+++ 01-clitest/src/app/pages/home/home.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent {
+  constructor(private authService: AuthService) {}
 }
