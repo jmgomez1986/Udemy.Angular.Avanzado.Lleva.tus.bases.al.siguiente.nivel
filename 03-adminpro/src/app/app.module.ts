00a8a0627b1a3a2afa86f60732da3cd0ede4bf9f
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-01-31 15:01:49 -0300

    Seccion 03: 19. Header, SiderBar, Breadcrumbs y contenedor principal
    Se pegan el html del template descargado a los componentes crrespondientes

--- 03-adminpro/src/app/app.module.ts
+++ 03-adminpro/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcumbComponent } from './shared/breadcumb/breadcumb.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 
@@ -17,7 +17,7 @@ import { HeaderComponent } from './shared/header/header.component';
     RegisterComponent,
     NotpagefoundComponent,
     DashboardComponent,
-    BreadcumbComponent,
+    BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent,
   ],
