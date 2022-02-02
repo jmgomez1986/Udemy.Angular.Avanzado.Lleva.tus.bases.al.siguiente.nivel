a11295cc22404933556c09d7bb85bf2e02c1da6f
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-02-02 17:02:56 -0300

    Seccion 03: 26. Página 404

--- 03-adminpro/src/app/pages/notpagefound/notpagefound.component.ts
+++ 03-adminpro/src/app/pages/notpagefound/notpagefound.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
-  styles: [
-  ]
+  styleUrls: ['notpagefound.component.css']
 })
 export class NotpagefoundComponent implements OnInit {
 
