8e878bd4dbdc23e9f7c98e2f279bcdceb315abdc
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-03-08 16:19:18 -0300

    Seccion 04: 36. AuthRoutingModule

--- 03-adminpro/src/app/app-routing.module.ts
+++ 03-adminpro/src/app/app-routing.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthRoutingModule } from './auth/auth.routing';
 import { PagesRoutingModule } from './pages/pages.routing';
-
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 
 import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
   { path: '**', component: NotpagefoundComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  imports: [RouterModule.forRoot(routes), AuthRoutingModule, PagesRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
