aab3969fd6e3f76a6ee88e64d23c4cef047f1983
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-03-10 18:06:54 -0300

    Seccion 06: 54. Graficas en Angular

--- 03-adminpro/src/app/pages/grafica1/grafica1.component.ts
+++ 03-adminpro/src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class Grafica1Component implements OnInit {
+  // Doughnut
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#6857E6', '#009FEE', '#F02059'],
+      },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- 03-adminpro/src/app/pages/pages.module.ts
+++ 03-adminpro/src/app/pages/pages.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from '../app-routing.module';
+import { NgChartsModule } from 'ng2-charts';
 
+import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 
@@ -27,6 +28,7 @@ import { PagesComponent } from './pages.component';
   imports: [
     CommonModule,
     FormsModule,
+    NgChartsModule,
     AppRoutingModule,
     SharedModule,
     ComponentsModule
