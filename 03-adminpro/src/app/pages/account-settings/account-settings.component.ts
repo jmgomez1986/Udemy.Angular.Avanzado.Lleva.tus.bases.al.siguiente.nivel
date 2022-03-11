6deaea69194adb63bd734f8ae047c99ea787ceef
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-03-11 14:38:03 -0300

    Seccion 07: 63. Cambiar el CSS principal de forma dinamica

--- 03-adminpro/src/app/pages/account-settings/account-settings.component.ts
+++ 03-adminpro/src/app/pages/account-settings/account-settings.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AccountSettingsComponent implements OnInit {
+  linkTheme = document.querySelector('#theme');
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`;
+
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
   }
 
 }
--- 03-adminpro/src/app/pages/pages.component.ts
+++ 03-adminpro/src/app/pages/pages.component.ts
@@ -3,14 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PagesComponent implements OnInit {
+  linkTheme = document.querySelector('#theme');
+  urlTheme: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.urlTheme =
+      localStorage.getItem('theme') ||
+      './assets/css/colors/default-dark.css';
+    this.linkTheme?.setAttribute('href', this.urlTheme);
   }
-
 }
