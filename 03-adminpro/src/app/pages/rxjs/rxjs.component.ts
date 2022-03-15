f9807bb5cfb31d1903ad288f5b6fb6631b7b90c0
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-03-15 16:57:43 -0300

    Seccion 08: 78. Metodo Retry de un Observable

--- 03-adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ 03-adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -18,12 +19,15 @@ export class RxjsComponent implements OnInit {
           observer.complete();
         }
         if ( i === 2 ) {
+          i = -1;
           observer.error('i llegó al valor 2');
         }
       }, 1000);
     });
 
-    obs$.subscribe(
+    obs$.pipe(
+      retry(1)
+    ).subscribe(
       (valor) => console.log('Subs: ', valor),
       (err) => console.warn('Error: ', err),
       () => console.info('Obs terminado')
