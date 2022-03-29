3b9ff2158712545f005d8eaf61582c6962f78e74
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-03-29 09:52:18 -0300

    Seccion 08: 84: Breadcumb usando observables y parametros de rutas

--- 03-adminpro/src/app/pages/pages.routing.ts
+++ 03-adminpro/src/app/pages/pages.routing.ts
@@ -14,12 +14,12 @@ const routes: Routes = [
     path: 'dashboard',
     component: PagesComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'charts', component: Grafica1Component },
-      { path: 'account-settings', component: AccountSettingsComponent },
-      { path: 'promises', component: PromesasComponent },
-      { path: 'rxjs', component: RxjsComponent }
+      { path: '', component: DashboardComponent, data: {title: 'Dashboard'} },
+      { path: 'progress', component: ProgressComponent, data: {title: 'ProgressBar'} },
+      { path: 'charts', component: Grafica1Component, data: {title: 'Charts'} },
+      { path: 'account-settings', component: AccountSettingsComponent, data: {title: 'Settings'} },
+      { path: 'promises', component: PromesasComponent, data: {title: 'Promises'} },
+      { path: 'rxjs', component: RxjsComponent, data: {title: 'RxJs'} }
     ],
   },
 ];
--- 03-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ 03-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BreadcrumbsComponent implements OnInit {
+  title: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.getRoutingData();
   }
 
+  getRoutingData() {
+    this.router.events.pipe(
+      filter((event: any) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map( (event: ActivationEnd) => event.snapshot.data)
+    ).subscribe((data: any) => {
+      this.title = data.title;
+      document.title = `AdminPro - ${this.title}`
+    });
+  }
 }
