3673da1e44925ebe3c27a1a2505c70eaf12c5b49
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-03-30 10:19:29 -0300

    Seccion 09: 92. Primera ruta

--- 04-backend-server/index.js
+++ 04-backend-server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
 // Crear el servidor de express
-const app =express();
+const app = express();
+
+// Rutas
+app.get( '/', (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'Hola Mundo!!!'
+  })
+} );
 
 app.listen(3000, () => {
   console.log('Servidor coriendo en el puerto 3000');
