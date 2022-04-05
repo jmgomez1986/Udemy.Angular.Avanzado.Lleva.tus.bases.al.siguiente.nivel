747d196927883a4e349c0a45b3fa21a0a9900540
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-05 12:28:35 -0300

    Seccion 09: 99. Configurar CORS

--- 04-backend-server/index.js
+++ 04-backend-server/index.js
@@ -1,10 +1,14 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
-const { dbConnection } = require('./database/config')
 
 // Crear el servidor de express
 const app = express();
+
+// Configurar CORS
+app.use(cors())
 
 // Base de datos
 dbConnection();
