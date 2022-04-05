b694497115657f8c2c1b2907e598a0a9d3c15550
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-05 16:40:39 -0300

    Seccion 10: 109. Validar que el correo electrónico sea único

--- 04-backend-server/controllers/usuarios.js
+++ 04-backend-server/controllers/usuarios.js
@@ -1,30 +1,47 @@
+const { response } = require('express');
+
 const Usuario = require('../models/usuario');
 
-const getUsuarios = async(req, res) => {
-
-  const usuarios = await Usuario.find({}, 'nombre email role google');
+const getUsuarios = async (req, res) => {
+	const usuarios = await Usuario.find({}, 'nombre email role google');
 
 	res.json({
 		ok: true,
-		usuarios
+		usuarios,
 	});
 };
 
-const crearUsuarios = async (req, res) => {
+const crearUsuarios = async (req, res = response) => {
+	const { nombre, password, email } = req.body;
 
-  const { nombre, password, email } = req.body;
+	try {
+		const existeEmail = await Usuario.findOne({ email });
 
-  const usuario = new Usuario(req.body);
+		if (existeEmail) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'El correo ya está regitrado',
+			});
+		}
 
-  await usuario.save();
+		const usuario = new Usuario(req.body);
 
-	res.json({
-		ok: true,
-		usuario
-	});
+		await usuario.save();
+
+		res.json({
+			ok: true,
+			usuario,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error inesperado....',
+		});
+	}
 };
 
 module.exports = {
 	getUsuarios,
-  crearUsuarios
+	crearUsuarios,
 };
