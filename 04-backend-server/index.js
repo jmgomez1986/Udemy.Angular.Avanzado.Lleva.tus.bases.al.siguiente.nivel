3ed0fc60eeaf5001e8aae6aef67705869097cb9e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-07 09:18:12 -0300

    Seccion 11: 124. CRUD y modelo de Médicos

--- 04-backend-server/index.js
+++ 04-backend-server/index.js
@@ -18,6 +18,7 @@ dbConnection();
 // Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/hospitales', require('./routes/hospitales'));
+app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/login', require('./routes/auth'));
 
 app.listen(3000, () => {
