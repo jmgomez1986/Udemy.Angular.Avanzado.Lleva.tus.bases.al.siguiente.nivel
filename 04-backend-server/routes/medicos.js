c6def1a3e4df0e7436f0eb3a2d7ac19986bc9143
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-07 15:45:32 -0300

    Seccion 11: 127. Validar MongoID

--- 04-backend-server/routes/medicos.js
+++ 04-backend-server/routes/medicos.js
@@ -23,6 +23,7 @@ router.post(
     validarJWT,
     check('nombre', 'El nombre del hospital es necesario').not().isEmpty(),
     check('hospital', 'El id del hospital es necesario').not().isEmpty(),
+    check('hospital', 'El id del hospital debe ser válido').isMongoId(),
     validarCampos
   ],
 	crearMedico
