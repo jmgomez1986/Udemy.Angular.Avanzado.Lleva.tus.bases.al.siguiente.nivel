ff2e241ff5686747ecec86472afcf209707886fd
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-07 08:32:51 -0300

    Seccion 11: 123. Inicio - CRUD de Médicos y Hospitales

---
+++ 04-backend-server/models/hospital.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const HospitalSchema = Schema({
+	nombre: {
+		type: String,
+		required: true,
+	},
+	img: {
+    type: String,
+  },
+  usuario: {
+    type: Schema.Types.ObjectId,
+    ref: 'Usuario'
+  }
+}, { collection: 'hospiteles'});
+
+HospitalSchema.method('toJSON', function() {
+  const { __v, ...object } = this.toObject();
+
+  object.uid = _id;
+  return object;
+});
+
+module.exports = model( 'Hospital', HospitalSchema);
