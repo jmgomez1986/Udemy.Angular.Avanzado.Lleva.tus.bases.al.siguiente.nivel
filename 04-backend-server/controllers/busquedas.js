ae1f65d55d1184c8f5dd4accc7ad3d162fb2ab67
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-12 09:23:09 -0300

    Seccion 11: 131. Búsqueda general en todas las colecciones de forma simultánea

--- 04-backend-server/controllers/busquedas.js
+++ 04-backend-server/controllers/busquedas.js
@@ -1,19 +1,31 @@
 const { response } = require('express');
 
-const getTodo = async(req, res = response) => {
-  const busqueda = req.params.busqueda;
-	// const hospitales = await Hospital.find().populate(
-	// 	'usuario',
-	// 	'nombre email img'
-	// );
+const Usuario = require('../models/usuario');
+const Medico = require('../models/medico');
+const Hospital = require('../models/hospital');
+
+const getTodo = async (req, res = response) => {
+	const busqueda = req.params.busqueda;
+	const regexp = new RegExp(busqueda, 'i');
+
+	// const usuarios = await Usuario.find({ nombre: regexp });
+	// const medicos = await Medico.find({ nombre: regexp });
+	// const hospitales = await Hospital.find({ nombre: regexp });
+
+	const [usuarios, medicos, hospitales] = await Promise.all([
+		Usuario.find({ nombre: regexp }),
+		Medico.find({ nombre: regexp }),
+		Hospital.find({ nombre: regexp }),
+	]);
 
 	res.json({
 		ok: true,
-		msg: 'getTodo',
-    busqueda
+		usuarios,
+		medicos,
+		hospitales,
 	});
-}
+};
 
 module.exports = {
-  getTodo
-}
+	getTodo,
+};
