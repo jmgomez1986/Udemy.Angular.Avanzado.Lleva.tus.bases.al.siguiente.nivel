060986adc148389ec91feae58d755bff110baf14
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-13 11:45:50 -0300

    Seccion 11: 134. Subir archivos al servidor - parte 2

--- 04-backend-server/controllers/uploads.js
+++ 04-backend-server/controllers/uploads.js
@@ -1,4 +1,6 @@
 const { response } = require('express');
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 // const Usuario = require('../models/usuario');
 // const Medico = require('../models/medico');
@@ -27,17 +29,49 @@ const fileUpLoad = async (req, res = response) => {
 
 	if (!req.files || Object.keys(req.files).length === 0) {
 		return res.status(400).json({
-      ok: false,
-      msg: 'No hay ningun archivo'
-    });
+			ok: false,
+			msg: 'No hay ningun archivo',
+		});
 	}
 
-  // Procesar la imagen ...
+	// Procesar la imagen ...
+	const file = req.files.imagen;
+	const nombreCortado = file.name.split('.');
+	const extensionArchivo = nombreCortado[nombreCortado.length - 1];
 
-	res.json({
-		ok: true,
-		msg: 'fileUpLoad',
+	// Validar extension
+	const extensionesValidas = ['png', 'jpg', 'gif'];
+
+	if (!extensionesValidas.includes(extensionArchivo)) {
+		return res.status(400).json({
+			ok: false,
+			msg: 'La extension del archivo no está permitida (*.png, *.jpg, *.gif)',
+		});
+	}
+
+	// Generar el nombre del archivo
+	const nombreArhivo = `${uuidv4()}.${extensionArchivo}`;
+
+	// Path para guardar la imagen
+	const path = `./uploads/${tipo}/${nombreArhivo}`;
+
+	// Mover la imagen
+	file.mv(path, function (err) {
+		if (err) {
+			console.log(err);
+			return res.status(400).json({
+				ok: false,
+				msg: 'La extension del archivo no está permitida (*.png, *.jpg, *.gif)',
+			});
+		}
+
+		res.json({
+			ok: true,
+			msg: 'Archivo subido',
+      nombreArhivo
+		});
 	});
+
 };
 
 module.exports = {
