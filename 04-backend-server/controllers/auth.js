2ade4769266083330e4b64f90badb8d1451b8562
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-18 15:09:43 -0300

    Seccion 12: 146. Validar token de Google en nuestro backend

--- 04-backend-server/controllers/auth.js
+++ 04-backend-server/controllers/auth.js
@@ -2,16 +2,15 @@ const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
-
-  const {email, password} = req.body;
+	const { email, password } = req.body;
 
 	try {
+		const userDB = await Usuario.findOne({ email });
 
-    const userDB = await Usuario.findOne({email});
-
-    // Verificar email
+		// Verificar email
 		if (!userDB) {
 			return res.status(404).json({
 				ok: false,
@@ -19,23 +18,23 @@ const login = async (req, res = response) => {
 			});
 		}
 
-    // Verificar contraseña
-    const validPassword = bcrypt.compareSync(password, userDB.password);
+		// Verificar contraseña
+		const validPassword = bcrypt.compareSync(password, userDB.password);
 
-    if (!validPassword) {
-      return res.status(404).json({
+		if (!validPassword) {
+			return res.status(404).json({
 				ok: false,
 				msg: 'Contraseña invalida',
 			});
-    }
+		}
 
-    // Generar el Token (JWT)
-    const token = await generarJWT(userDB.id);
+		// Generar el Token (JWT)
+		const token = await generarJWT(userDB.id);
 
-    res.json({
+		res.json({
 			ok: true,
 			msg: 'Logueado exitosamente',
-      token
+			token,
 		});
 	} catch (error) {
 		console.log(error);
@@ -46,6 +45,28 @@ const login = async (req, res = response) => {
 	}
 };
 
+const googleSignIn = async (req, res = reponse) => {
+	const googleToken = req.body.token;
+
+	try {
+		const { name, email, picture } = await googleVerify(googleToken);
+
+		res.json({
+			ok: true,
+			msg: 'Google Sign-in',
+			name,
+			email,
+			picture,
+		});
+	} catch (error) {
+		res.status(401).json({
+			ok: false,
+			msg: 'El token no es correcto',
+		});
+	}
+};
+
 module.exports = {
 	login,
+	googleSignIn,
 };
