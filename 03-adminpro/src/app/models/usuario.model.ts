45eca81f74f43cfe52510d3c330cee20dd9832ba
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-19 12:19:18 -0300

    Seccion 14: 162. Creando un modelo de usuario

---
+++ 03-adminpro/src/app/models/usuario.model.ts
@@ -0,0 +1,11 @@
+export class Usuario {
+  construcor(
+    nombre: string,
+    email: string,
+    password?: string,
+    img?: string,
+    google?: string,
+    role?: string,
+    uid?: string
+  ) { }
+}
