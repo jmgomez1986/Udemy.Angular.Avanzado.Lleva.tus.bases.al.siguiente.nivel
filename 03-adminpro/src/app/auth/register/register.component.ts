23f843af2a179b6f3a962e9ca08f5b7e191f8962
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-19 16:02:23 -0300

    Seccion 14: 164. Validaciones del formulario

--- 03-adminpro/src/app/auth/register/register.component.ts
+++ 03-adminpro/src/app/auth/register/register.component.ts
@@ -4,25 +4,31 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['register.component.css']
+  styleUrls: ['register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public formSubmitted = false;
 
   public registerForm = this.fb.group({
-    nombre: ['Matias', [Validators.required, Validators.minLength(3)]],
-    email: ['test01@gmail.com', Validators.required],
-    password: ['123456', Validators.required],
-    password2: ['123456', Validators.required],
-    terminos: [false, Validators.required]
+    nombre: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terminos: [false, Validators.requiredTrue],
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   crearUsuario() {
+    this.formSubmitted = true;
     console.log('Formulario: ', this.registerForm.value);
   }
 
+  campoNoValido(campo: string): boolean {
+    return this.registerForm.get(campo)?.invalid && this.formSubmitted
+      ? true
+      : false;
+  }
 }
