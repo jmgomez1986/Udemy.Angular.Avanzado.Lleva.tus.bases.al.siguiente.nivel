f760b37010c8f0efb8b7adb65b86b1678d2ca307
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-20 15:05:55 -0300

    Seccion 14: 173. Obtener el Token de una autenticación, Google Sign-In

--- 03-adminpro/src/app/auth/login/login.component.ts
+++ 03-adminpro/src/app/auth/login/login.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 import { UsuarioService } from '../../services/usuario.service';
 
+declare const gapi: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public loginForm = this.fb.group({
-    email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    email: [
+      localStorage.getItem('email') || '',
+      [Validators.required, Validators.email],
+    ],
     password: ['', Validators.required],
     remember: [false],
   });
@@ -23,12 +28,15 @@ export class LoginComponent {
     private usuarioService: UsuarioService
   ) {}
 
-  login() {
+  ngOnInit(): void {
+    this.renderButton();
+  }
 
+  login() {
     this.usuarioService.login(this.loginForm.value).subscribe({
       next: (resp) => {
         console.log('Response: ', resp);
-        if ( this.loginForm.get('remember')?.value) {
+        if (this.loginForm.get('remember')?.value) {
           localStorage.setItem('email', this.loginForm.get('email')?.value);
         } else {
           localStorage.removeItem('email');
@@ -38,6 +46,29 @@ export class LoginComponent {
       error: (err) => {
         Swal.fire('Error!', err.error.msg, 'error');
       },
+    });
+  }
+
+  onSuccess(googleUser) {
+    const token = googleUser.getAuthResponse().id_token;
+
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    console.log('Token: ', token);
+  }
+
+  onFailure(error) {
+    console.log(error);
+  }
+
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+      onsuccess: this.onSuccess,
+      onfailure: this.onFailure,
     });
   }
 }
