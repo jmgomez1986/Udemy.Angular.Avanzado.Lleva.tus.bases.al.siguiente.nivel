7b9cd78b0602327f1bf19100403ccc78c21bca7e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-22 09:20:48 -0300

    Seccion 15: 186. Crear el componente del perfil del usuario

--- 03-adminpro/src/app/pages/pages.routing.ts
+++ 03-adminpro/src/app/pages/pages.routing.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { AuthGuard } from '../guards/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -8,7 +9,7 @@ import { PagesComponent } from './pages.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
-import { AuthGuard } from '../guards/auth.guard';
+import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
   {
@@ -16,18 +17,39 @@ const routes: Routes = [
     component: PagesComponent,
     canActivate: [AuthGuard],
     children: [
-      { path: '', component: DashboardComponent, data: {title: 'Dashboard'} },
-      { path: 'progress', component: ProgressComponent, data: {title: 'ProgressBar'} },
-      { path: 'charts', component: Grafica1Component, data: {title: 'Charts'} },
-      { path: 'account-settings', component: AccountSettingsComponent, data: {title: 'Settings'} },
-      { path: 'promises', component: PromesasComponent, data: {title: 'Promises'} },
-      { path: 'rxjs', component: RxjsComponent, data: {title: 'RxJs'} }
+      { path: '', component: DashboardComponent, data: { title: 'Dashboard' } },
+      {
+        path: 'progress',
+        component: ProgressComponent,
+        data: { title: 'ProgressBar' },
+      },
+      {
+        path: 'charts',
+        component: Grafica1Component,
+        data: { title: 'Charts' },
+      },
+      {
+        path: 'account-settings',
+        component: AccountSettingsComponent,
+        data: { title: 'Settings' },
+      },
+      {
+        path: 'promises',
+        component: PromesasComponent,
+        data: { title: 'Promises' },
+      },
+      { path: 'rxjs', component: RxjsComponent, data: { title: 'RxJs' } },
+      {
+        path: 'profile',
+        component: PerfilComponent,
+        data: { title: 'Profile' },
+      },
     ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class PagesRoutingModule {}
