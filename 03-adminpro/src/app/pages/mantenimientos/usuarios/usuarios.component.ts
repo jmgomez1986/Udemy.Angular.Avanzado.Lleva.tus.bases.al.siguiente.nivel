0e96503f1f4f2f1694b7dda9337d50936b89725e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-25 11:27:20 -0300

    Seccion 16: 200. Paginar los usuarios

--- 03-adminpro/src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ 03-adminpro/src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -5,21 +5,38 @@ import { Usuario } from './../../../models/usuario.model';
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class UsuariosComponent implements OnInit {
   totalUsuarios: number = 0;
   usuarios: Usuario[] = [];
+  desde: number = 0;
+  limite: number = 5;
 
-  constructor(private usuarioService: UsuarioService) { }
+  constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
-    this.usuarioService.cargarUsuarios(0, 5)
-      .subscribe(({totalReg, usuarios}) => {
-        this.totalUsuarios = totalReg;
-        this.usuarios = usuarios;
-    })
+    this.cargarUsuarios();
   }
 
+  cargarUsuarios() {
+    this.usuarioService
+      .cargarUsuarios(this.desde, this.limite)
+      .subscribe(({ totalReg, usuarios }) => {
+        this.totalUsuarios = totalReg;
+        this.usuarios = usuarios;
+      });
+  }
+
+  cambiarPagina(valor: number) {
+    this.desde += valor;
+
+    if (this.desde < 0) {
+      this.desde = 0;
+    } else if (this.desde >= this.totalUsuarios) {
+      this.desde -= valor;
+    }
+
+    this.cargarUsuarios();
+  }
 }
