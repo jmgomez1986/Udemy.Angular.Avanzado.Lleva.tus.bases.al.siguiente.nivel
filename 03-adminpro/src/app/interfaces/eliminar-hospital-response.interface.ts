f943add0865ac9fe44f196f9ae7fc259ab17188f
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-26 19:24:25 -0300

    Seccion 17: 220. CRUD de Hospitales

---
+++ 03-adminpro/src/app/interfaces/eliminar-hospital-response.interface.ts
@@ -0,0 +1,4 @@
+export interface EliminarHospitalResponse {
+  ok: boolean;
+  msg: string;
+}
--- 03-adminpro/src/app/services/hospital.service.ts
+++ 03-adminpro/src/app/services/hospital.service.ts
@@ -4,15 +4,17 @@ import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { HeadersHttp } from '../interfaces/headers.interface';
 import { CargarHospitalesResponse } from '../interfaces/cargar-hospitales-response.interface';
+import { CrearHospitalResponse } from '../interfaces/crear-hospital-response.interface';
+import { ActualizarHospitalResponse } from '../interfaces/actualizar-hospital-response.interface';
+import { EliminarHospitalResponse } from '../interfaces/eliminar-hospital-response.interface';
 
 const BASE_URL = environment.baseUrl;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HospitalService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   get token(): string {
     return localStorage.getItem('token') || '';
@@ -27,8 +29,30 @@ export class HospitalService {
   cargarHospitales(desde = 0, limite = 5) {
     // const url = `${BASE_URL}/hospitales?desde=${desde}&limite=${limite}`;
     const url = `${BASE_URL}/hospitales`;
-    return this.http.get<CargarHospitalesResponse>(url, this.headers).pipe(
-      map( resp => resp.hospitales)
+    return this.http
+      .get<CargarHospitalesResponse>(url, this.headers)
+      .pipe(map((resp) => resp.hospitales));
+  }
+
+  crearHospital(nombre: string) {
+    const url = `${BASE_URL}/hospitales`;
+    return this.http.post<CrearHospitalResponse>(url, { nombre }, this.headers);
+  }
+
+  actualizarHospital(id: string, nombre: string) {
+    const url = `${BASE_URL}/hospitales/${id}`;
+    return this.http.put<ActualizarHospitalResponse>(
+      url,
+      { nombre },
+      this.headers
+    );
+  }
+
+  eliminarHospital(id: string) {
+    const url = `${BASE_URL}/hospitales/${id}`;
+    return this.http.delete<EliminarHospitalResponse>(
+      url,
+      this.headers
     );
   }
 }
