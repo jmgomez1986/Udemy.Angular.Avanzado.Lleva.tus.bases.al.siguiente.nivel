b64d1f03aeb402249160368ace4fd63f05bafc14
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-27 09:59:31 -0300

    Seccion 17: 223. Componente de Médico, Médicos y Servicio de Médicos

--- 03-adminpro/src/app/pages/pages.module.ts
+++ 03-adminpro/src/app/pages/pages.module.ts
@@ -18,6 +18,7 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
 import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
 import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
+import { MedicoComponent } from './mantenimientos/medicos/medico.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.comp
     UsuariosComponent,
     MedicosComponent,
     HospitalesComponent,
+    MedicoComponent,
   ],
   exports: [
     DashboardComponent,
--- 03-adminpro/src/app/pages/pages.routing.ts
+++ 03-adminpro/src/app/pages/pages.routing.ts
@@ -14,6 +14,7 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
 import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
 import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
+import { MedicoComponent } from './mantenimientos/medicos/medico.component';
 
 const routes: Routes = [
   {
@@ -57,6 +58,11 @@ const routes: Routes = [
       {
         path: 'medicos',
         component: MedicosComponent,
+        data: { title: 'Mantenimiendo de medicos' },
+      },
+      {
+        path: 'medicos/:id',
+        component: MedicoComponent,
         data: { title: 'Mantenimiendo de medicos' },
       },
       {
