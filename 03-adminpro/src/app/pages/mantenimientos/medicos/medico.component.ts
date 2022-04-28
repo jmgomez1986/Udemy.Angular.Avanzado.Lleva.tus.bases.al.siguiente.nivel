9a15407607510747d83beef113c1f63bffa061ec
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-04-28 08:44:58 -0300

    Seccion 17: 228. Preparar el formulario de creación de médicos

--- 03-adminpro/src/app/pages/mantenimientos/medicos/medico.component.ts
+++ 03-adminpro/src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HospitalService } from './../../../services/hospital.service';
+import { Hospital } from '../../../models/hospital.model';
 
 @Component({
   selector: 'app-medico',
   templateUrl: './medico.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class MedicoComponent implements OnInit {
+  public medicoForm: FormGroup;
+  public hospitales: Hospital[] = [];
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private hospitalService: HospitalService) {}
 
   ngOnInit(): void {
+    this.medicoForm = this.fb.group({
+      nombre: ['Matias', Validators.required],
+      hospital: ['', Validators.required],
+    });
+
+    this.cargarHospitales();
   }
 
+  cargarHospitales () {
+    this.hospitalService.cargarHospitales().subscribe((hospitales) => {
+      console.log('Hospitales: ', hospitales);
+      this.hospitales = hospitales;
+    });
+  }
+
+  guardarMedico() {
+    console.log('Form: ', this.medicoForm);
+  }
 }
