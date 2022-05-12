75a5f07277f2cf610c514cc34e289789ce564eb3
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-12 09:27:00 -0300

    Secion 22: 277. BeforeEach - BeforeAll - AfterAll - After Each

--- pruebas/src/app/basicas/clases/clases.spec.ts
+++ pruebas/src/app/basicas/clases/clases.spec.ts
@@ -2,19 +2,28 @@ import { Jugador } from "./clases";
 
 describe('Pruebas de clase', () => {
 
-  const jugador = new Jugador();
+  let jugador = new Jugador();
+
+  beforeEach(() => {
+    // jugador.hp = 100;
+    jugador = new Jugador();
+  });
 
   it('Debe de retornar 80 de hp si recibe 20 de daño', () => {
-    // const jugador = new Jugador();
     const resp = jugador.recibeDanio(20);
 
     expect(resp).toBe(80);
   });
 
   it('Debe de retornar 50 de hp si recibe 50 de daño', () => {
-    // const jugador = new Jugador();
     const resp = jugador.recibeDanio(50);
 
     expect(resp).toBe(50);
+  });
+
+  it('Debe de retornar 100 de hp si recibe 100 de daño', () => {
+    const resp = jugador.recibeDanio(100);
+
+    expect(resp).toBe(0);
   });
 });
