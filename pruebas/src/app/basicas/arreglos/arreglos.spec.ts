5d538d906a584a0dd415467500182c19e28e9849
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-16 23:54:06 -0300

    Seccion 23: 282. Saltar pruebas

--- pruebas/src/app/basicas/arreglos/arreglos.spec.ts
+++ pruebas/src/app/basicas/arreglos/arreglos.spec.ts
@@ -1,13 +1,15 @@
 import { obtenerRobots } from "./arreglos";
 
-describe('Pruebas de arreglos', () => {
+// x en el describe saltea todas las pruebas
+xdescribe('Pruebas de arreglos', () => {
   it('Debe de retornar al menos 3 robots', () => {
     const resp = obtenerRobots();
 
     expect(resp.length).toBeGreaterThanOrEqual(3);
   });
 
-  it('Debe de existir "Megaman y "Ultron"', () => {
+  // x en el it saltea la prueba
+  xit('Debe de existir "Megaman y "Ultron"', () => {
     const resp = obtenerRobots();
 
     expect(resp).toContain('Megaman');
