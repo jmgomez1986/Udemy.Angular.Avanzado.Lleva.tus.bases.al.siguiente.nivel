8750bc942f5c2d1e1590240cfe07519a7de5609c
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-18 12:18:19 -0300

    Seccion 23: 287. Explicación del contenido de los archivos adjuntos

---
+++ pruebas/src/app/intermedio/espias/medicos.component.spec.ts
@@ -0,0 +1,10 @@
+import { MedicosComponent } from './medicos.component';
+import { MedicosService } from './medicos.service';
+
+describe('MedicosComponent', () => {
+  let componente: MedicosComponent;
+
+  beforeEach(() => {});
+
+  it('', () => {});
+});
