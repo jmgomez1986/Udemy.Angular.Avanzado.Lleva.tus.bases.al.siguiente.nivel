945de9f44d0552fa126087d4bb69392e609d5f20
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-19 00:42:43 -0300

    Seccion 23: 288.Espías

--- pruebas/src/app/intermedio/espias/medicos.component.spec.ts
+++ pruebas/src/app/intermedio/espias/medicos.component.spec.ts
@@ -1,10 +1,37 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { from } from 'rxjs';
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
 
 describe('MedicosComponent', () => {
   let componente: MedicosComponent;
+  let servicio: MedicosService;
 
-  beforeEach(() => {});
+  beforeEach(() => {
+    // Forma 1
+    const spy = jasmine.createSpyObj('HttpClient', ['prueba']); //Hacemos un fake
+    servicio = new MedicosService(spy);
+    componente = new MedicosComponent(servicio);
 
-  it('', () => {});
+    // Forma 2
+    // TestBed.configureTestingModule({
+    //   providers: [HttpClient, HttpHandler, MedicosService],
+    // });
+
+    // const fixture = TestBed.createComponent(MedicosComponent);
+    // componente = fixture.componentInstance;
+    // servicio = fixture.debugElement.injector.get(MedicosService);
+
+  });
+
+  it('Debe de cargar los médicos', () => {
+    const medicos = ['medico1', 'medico2', 'medico3'];
+
+    spyOn(servicio, 'getMedicos').and.callFake(() => {
+      return from([medicos]);
+    });
+    componente.ngOnInit();
+    expect(componente.medicos.length).toBeGreaterThan(0);
+  });
 });
--- pruebas/src/app/intermedio/espias/medicos.service.ts
+++ pruebas/src/app/intermedio/espias/medicos.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class MedicosService {
-  constructor(public http: Http) {}
+  constructor(public http: HttpClient) {}
 
   getMedicos() {
-    return this.http.get('...').map((resp) => resp['medicos']);
+    return this.http.get('...').pipe(map((resp: any) => resp.medicos));
   }
 
   agregarMedico(medico: any) {
-    return this.http.post('...', medico).map((resp) => resp['medico']);
+    return this.http
+      .post('...', medico)
+      .pipe(map((resp: any) => resp - medico));
   }
 
   borrarMedico(id: string) {
-    return this.http.delete('...').map((resp) => resp['medico']);
+    return this.http.delete('...').pipe(map((resp: any) => resp.medico));
   }
 }
