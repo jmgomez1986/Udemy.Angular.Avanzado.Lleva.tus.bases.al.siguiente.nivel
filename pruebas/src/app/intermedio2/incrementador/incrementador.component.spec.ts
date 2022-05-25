faf3f53e07d69d4bb16128aa624b19500ce8426e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-25 13:39:42 -0300

    Seccion 24: 303. Revisar el valor de un input físicamente

--- pruebas/src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ pruebas/src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('Incremendator Component', () => {
   let component: IncrementadorComponent;
@@ -29,5 +30,17 @@ describe('Incremendator Component', () => {
     const element: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
 
     expect(element.innerHTML).toContain(component.leyenda);
-  })
+  });
+
+  it('Debe de mostrar en el input el valor del progreso', () => {
+    component.cambiarValor(55);
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      const input: DebugElement = fixture.debugElement.query(By.css('input'));
+      const element: HTMLInputElement  = input.nativeElement;
+
+      expect(element.value).toBe('55');
+    })
+  });
 });
