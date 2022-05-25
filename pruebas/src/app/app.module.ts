0c2e48bf38b0520f9fbddb931d0be6a19647553a
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-25 00:29:49 -0300

    Seccion 24: 299. Archivos SPEC generados automáticamente usando AngularCLI

--- pruebas/src/app/app.module.ts
+++ pruebas/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
+import { HospitalComponent } from './intermedio2/hospital/hospital.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicosComponent,
-    MedicoComponent
+    MedicoComponent,
+    HospitalComponent
   ],
   imports: [
     BrowserModule
