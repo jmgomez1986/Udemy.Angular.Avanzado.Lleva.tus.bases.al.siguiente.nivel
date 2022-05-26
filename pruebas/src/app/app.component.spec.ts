5bfcbd97daa35d1d0bf9b0c7f2a53a1006e34500
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-26 17:05:56 -0300

    Seccion 25: 313. Confirmar que exista un routerLink hacia el componente de médicos

--- pruebas/src/app/app.component.spec.ts
+++ pruebas/src/app/app.component.spec.ts
@@ -3,17 +3,13 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkWithHref, RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-      imports: [
-        RouterTestingModule.withRoutes([])
-      ],
+      declarations: [AppComponent],
+      imports: [RouterTestingModule.withRoutes([])],
     }).compileComponents();
   });
 
@@ -29,11 +25,32 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('pruebas');
   });
 
-  it('Debe de tener un roter-outlet', () => {
+  it('Debe de tener un router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
     expect(debugElement).not.toBeNull();
   });
 
+  it('Debe de tener un link a la pagina de medicos', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const debugElements = fixture.debugElement.queryAll(
+      By.directive(RouterLinkWithHref)
+    );
+
+    // let existe = false;
+
+    // for (const elem of debugElements) {
+    //   if (elem.attributes['routerLink'] === '/medicos') {
+    //     existe = true;
+    //     break;
+    //   }
+    // }
+    // expect(existe).toBeTruthy();
+
+    const index = debugElements.findIndex(
+      (de) => de.attributes['routerLink'] === '/medicos'
+    );
+
+    expect(index).toBeGreaterThanOrEqual(0);
+  });
 });
