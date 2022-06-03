d4640c6fc160e49f3d491409651b1925e08d5302
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-06-02 23:48:19 -0300

    Seccion 26: 325. Cambiar valores de la gráfica de forma aleatoria

--- 05-goty/src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ 05-goty/src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,29 +1,28 @@
-import { Component} from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
-  styleUrls: ['./grafico-barra-horizontal.component.scss']
+  styleUrls: ['./grafico-barra-horizontal.component.scss'],
 })
-export class GraficoBarraHorizontalComponent {
-
+export class GraficoBarraHorizontalComponent implements OnDestroy {
   results: any[] = [
     {
-      "name": "Juego 1",
-      "value": 20
+      name: 'Juego 1',
+      value: 20,
     },
     {
-      "name": "Juego 2",
-      "value": 25
+      name: 'Juego 2',
+      value: 25,
     },
     {
-      "name": "Juego 3",
-      "value": 15
+      name: 'Juego 3',
+      value: 15,
     },
     {
-      "name": "Juego 4",
-      "value": 30
-    }
+      name: 'Juego 4',
+      value: 30,
+    },
   ];
 
   // options
@@ -38,10 +37,27 @@ export class GraficoBarraHorizontalComponent {
 
   colorScheme = 'nightLights';
 
-  constructor() { }
+  intervalo: any;
+
+  constructor() {
+    this.intervalo = setInterval(() => {
+      console.log('tick');
+
+      const newResults = [...this.results];
+
+      for (let i in newResults) {
+        newResults[i].value = Math.round(Math.random() * 500);
+      }
+
+      this.results = [...newResults];
+    }, 1500);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval( this.intervalo );
+  }
 
   onSelect(event: any) {
     console.log(event);
   }
-
 }
