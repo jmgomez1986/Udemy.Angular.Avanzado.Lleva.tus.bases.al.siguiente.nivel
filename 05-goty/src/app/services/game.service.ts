98d5562fc67e72dc6c70af20b223153003dc7827
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-06-12 02:29:13 -0300

    Seccion 26: 334. Mostrar los juegos en el HTML

--- 05-goty/src/app/services/game.service.ts
+++ 05-goty/src/app/services/game.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
@@ -8,10 +10,20 @@ import { Game } from '../interfaces/interfaces';
   providedIn: 'root',
 })
 export class GameService {
+  private juegos: Game[] = [];
 
   constructor(private http: HttpClient) {}
 
   getNominados() {
-    return this.http.get<Game[]>(`${environment.url}/api/goty`);
+    if (this.juegos.length > 0) {
+      // no tenemos juegos
+      console.log('Desde caché');
+      return of(this.juegos);
+    } else {
+      console.log('Desde Internet');
+      return this.http
+        .get<Game[]>(`${environment.url}/api/goty`)
+        .pipe(tap((juegos) => (this.juegos = juegos)));
+    }
   }
 }
