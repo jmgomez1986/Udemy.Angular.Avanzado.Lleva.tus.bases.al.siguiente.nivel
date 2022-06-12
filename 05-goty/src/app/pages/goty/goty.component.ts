79fcaf3934d0dee80c5c567f3c28ad6ffb8536d5
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-06-12 02:12:12 -0300

    Seccion 26: 333. Angular: HTTP GET - Obtener y mostrar los juegos

--- 05-goty/src/app/pages/goty/goty.component.ts
+++ 05-goty/src/app/pages/goty/goty.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../../interfaces/interfaces';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-goty',
   templateUrl: './goty.component.html',
-  styleUrls: ['./goty.component.scss']
+  styleUrls: ['./goty.component.scss'],
 })
 export class GotyComponent implements OnInit {
+  juegos: Game[] = [];
 
-  constructor() { }
+  constructor(private gameService: GameService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.gameService.getNominados().subscribe((juegos) => {
+      console.log(juegos);
+      this.juegos = juegos;
+    });
   }
-
 }
