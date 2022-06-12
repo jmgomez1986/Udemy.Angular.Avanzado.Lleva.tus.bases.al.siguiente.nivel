0ec8b8aed89f80d65b7a1baf2f2903aa98666a12
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-06-12 01:32:52 -0300

    Seccion 26: 331. Post: Incrementar en 1 el voto del juego

--- 06-goty-backend/functions/.eslintrc.js
+++ 06-goty-backend/functions/.eslintrc.js
@@ -1,34 +1,62 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    '.eslintrc.js'
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
-  rules: {
-    "quotes": ["warn", "single"],
-    indent: ['warn', 2, { SwitchCase: 1 }],
-    "import/no-unresolved": 0,
-  },
+	root: true,
+	env: {
+		es6: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'google',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	ignorePatterns: [
+		'/lib/**/*', // Ignore built files.
+		'.eslintrc.js',
+	],
+	plugins: ['@typescript-eslint', 'import'],
+	rules: {
+		'import/no-unresolved': 0,
+
+		////////// Stylistic Issues //////////
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+		'object-curly-spacing': [0, 'allways'],
+		'brace-style': 2, // enforce one true brace style (off by default)
+		camelcase: 2, // require camel case names
+		'comma-spacing': 0, // enforce spacing before and after comma
+		'comma-style': [2, 'last'], // enforce one true comma style (off by default)
+		'eol-last': 1, // enforce newline at the end of file, with no multiple empty lines
+		'func-names': 1, // require function expressions to have a name (off by default)
+		'func-style': [1, 'declaration'], // enforces use of function declarations or expressions (off by default)
+		'no-inline-comments': 0, // disallow comments inline after code (off by default)
+		'no-lonely-if': 0, // disallow if as the only statement in an else block (off by default)
+		'no-mixed-spaces-and-tabs': 2, // disallow mixed spaces and tabs for indentation
+		'no-multiple-empty-lines': 1, // disallow multiple empty lines (off by default)
+		'no-nested-ternary': 0, // disallow nested ternary expressions (off by default)
+		'semi-spacing': 2, // disallow space before semicolon
+		'no-spaced-func': 2, // disallow space between function identifier and application
+		'no-ternary': 0, // disallow the use of ternary operators (off by default)
+		'no-trailing-spaces': 1, // disallow trailing whitespace at the end of lines
+		'no-underscore-dangle': 0, // disallow dangling underscores in identifiers
+		'one-var': 0, // allow just one var statement per function (off by default)
+		'padded-blocks': 0, // enforce padding within blocks (off by default)
+		'quote-props': [2, 'as-needed'], // require quotes around object literal property names (off by default)
+		quotes: [1, 'single'], // specify whether double or single quotes should be used
+		semi: [1, 'always'], // require or disallow use of semicolons instead of ASI
+		'sort-vars': 0, // sort variables within the same declaration block (off by default)
+		'space-after-function-name': 0, // require a space after function names (off by default)
+		'space-after-keywords': 0, // require a space after certain keywords (off by default)
+		'space-before-blocks': 0, // require or disallow space before blocks (off by default)
+		'space-in-brackets': [0, 'allways'], // require or disallow spaces inside brackets (off by default)
+		'spaced-line-comment': 0, // require or disallow a space immediately following the // in a line comment (off by default)
+	},
 };
--- 06-goty-backend/functions/src/index.ts
+++ 06-goty-backend/functions/src/index.ts
@@ -8,42 +8,62 @@ import * as cors from 'cors';
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+	credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', {structuredData: true});
-  response.json({
-    mensaje: 'Hello from Firebase!!!!!!!',
-  });
+	functions.logger.info('Hello logs!', { structuredData: true });
+	response.json({
+		mensaje: 'Hello from Firebase!!!!!!!',
+	});
 });
 
 export const getGOTY = functions.https.onRequest(async (request, response) => {
-  // const nombre = request.query.nombre || 'Sin Nombre';
+	// const nombre = request.query.nombre || 'Sin Nombre';
 
-  // response.json( nombre );
-  const gotyRef = db.collection('goty');
-  const docsSnap = await gotyRef.get();
-  const juegos = docsSnap.docs.map( (doc) => doc.data() );
+	// response.json( nombre );
+	const gotyRef = db.collection('goty');
+	const docsSnap = await gotyRef.get();
+	const juegos = docsSnap.docs.map((doc) => doc.data());
 
-  response.json( juegos );
+	response.json(juegos);
 });
 
 // Express
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 app.get('/goty', async (req, res) => {
-  const gotyRef = db.collection('goty');
-  const docsSnap = await gotyRef.get();
-  const juegos = docsSnap.docs.map( (doc) => doc.data() );
+	const gotyRef = db.collection('goty');
+	const docsSnap = await gotyRef.get();
+	const juegos = docsSnap.docs.map((doc) => doc.data());
 
-  res.json( juegos );
+	res.json(juegos);
+});
+
+app.post('/goty/:id', async (req, res) => {
+	const id = req.params.id;
+	const gameRef = db.collection('goty').doc(id);
+	const gameSnap = await gameRef.get();
+
+	if (!gameSnap.exists) {
+		res.status(404).json({
+			ok: false,
+			mensaje: 'No existe un juego con ese ID ' + id,
+		});
+	} else {
+		const antes = gameSnap.data() || { votos: 0 };
+		await gameRef.update({
+			votos: antes.votos + 1,
+		});
+
+		res.json({
+			ok: true,
+			mensaje: `Gracias por tu voto a ${antes.name}`,
+		});
+	}
 });
 
 export const api = functions.https.onRequest(app);
